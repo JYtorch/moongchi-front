072fda176b31fa672d1520fed84ebe94c66dd552
JYA <abogado2113@gmail.com>
2022-11-03 02:17:57 +0900

    Add Preloader

---
+++ src/views/Preloader.vue
@@ -0,0 +1,21 @@
+<template>
+    <!--preloading-->
+    <div id="preloader">
+        <img class="logo" src="../assets/뭉치.png" alt="" style="border-radius : 30%">
+        <div id="status">
+            <span></span>
+            <span></span>
+        </div>
+    </div>
+    <!--end of preloading-->
+</template>
+
+<script>
+export default {
+    name: "Preloader"
+}
+</script>
+
+<style scoped>
+
+</style>
